fc63131bfd0cd5de6636ea29db0890ff2dd30af5
hazeljw <hazel@multitudes.co>
2024-10-13 11:22:11 +1300

    feat: landing screen skeleton

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import LandingScreen from './components/landingScreen/LandingScreen';
+
+enum GameStatus {
+  NotJoined,
+  InLobby,
+  InGame
+}
 
 function App() {
+  const [gameStatus, setGameStatus] = React.useState(GameStatus.NotJoined);
+
   return (
     <div className="App">
+
+      { gameStatus === GameStatus.NotJoined && <LandingScreen />}
       
     </div>
   );
--- client/src/components/landingScreen/LandingScreen.tsx
+++ client/src/components/landingScreen/LandingScreen.tsx
@@ -1,14 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './styles.css';
+import { Avatar, Box, Button, TextField } from '@mui/material';
 
 function LandingScreen() {
-  return (
-    <div className="LandingScreen">
 
-        
-      
-    </div>
+    const handleHostNewGame = () => {
+    }
+
+    const handleJoinGame = () => {
+    }
+
+  return (
+    <Box className="LandingScreen flexCenter" gap={3} flexDirection={'column'}>
+        <Box className="title">LingoSnap</Box>
+
+        <Box className="flexCenter"  gap={1} >
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+            <TextField id="outlined-basic" label="Name" variant="outlined" />
+        </Box>
+
+        <Button variant="contained" color="primary" onClick={handleHostNewGame}>Host New Game</Button>
+
+        <Box className="flexCenter" gap={1}>
+            <TextField id="outlined-basic" label="Game Code" variant="outlined" />
+            <Button variant="contained" color="primary" onClick={handleJoinGame}>Join Game</Button>
+        </Box>
+
+    </Box>
   );
 }
 
