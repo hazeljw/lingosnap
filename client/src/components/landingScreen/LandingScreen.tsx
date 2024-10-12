c2dc25decf9143a127ec0bb543af645988a20453
hazeljw <hazel@multitudes.co>
2024-10-13 11:07:08 +1300

    feat: typescript + mui

---
+++ client/src/components/landingScreen/LandingScreen.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './styles.css';
+
+function LandingScreen() {
+  return (
+    <div className="LandingScreen">
+
+        
+      
+    </div>
+  );
+}
+
+export default LandingScreen;
