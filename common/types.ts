fea77cfd5c7260e73bac9be9bcc3fe136727f5e3
hazeljw <hazel@multitudes.co>
2024-10-20 18:51:39 +1300

    feat: images and content data

--- common/types.ts
+++ common/types.ts
@@ -2,6 +2,7 @@ export interface User {
     id: string;
     name: string;
     isHost: boolean;
+    selectedLanguage?: Language;
 }
 
 export interface RoomData {
@@ -9,4 +10,18 @@ export interface RoomData {
     hostId?: string;
     users?: User[];
     gameState?: any;
+}
+
+export enum Language {
+    English = 'English',
+    Spanish = 'Spanish',
+    French = 'French',
+    German = 'German',
+    Italian = 'Italian',
+    Dutch = 'Dutch',
+    Portuguese = 'Portuguese',
+    Japanese = 'Japanese',
+    Korean = 'Korean',
+    Finnish = 'Finnish',
+    TeReo = 'Te Reo',
 }
