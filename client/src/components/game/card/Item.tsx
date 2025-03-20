0c06edf72238b1699682f85b53517f1ba54fefc9
Noah <hazeljadew@gmail.com>
2025-03-20 14:20:06 +1300

    fix: fix animation key name preventing correct boundary

--- client/src/components/game/card/Item.tsx
+++ client/src/components/game/card/Item.tsx
@@ -26,24 +26,26 @@ const Item = ({item, containerWidth, containerHeight}:ItemProps) => {
 
     const {xSpeed, ySpeed, zSpeed} = item.motion
 
+    const animationKey = `size-${item.size}`
+
     return (
         <div>
             <style>
                 {`
-                    @keyframes x {
+                    @keyframes ${animationKey}-x {
                        100% {
                             transform: translateX(calc(${containerWidth} - ${item.size}));
                         }
                     }
-                    @keyframes y {
+                    @keyframes ${animationKey}-y {
                         100% {
                             transform: translateY(calc(${containerHeight} - ${item.size}));
                         }
                     }
                 `}
             </style>
-            <div className="el-wrap" style={{animation: `x ${xSpeed}s linear infinite alternate`}}>
-                <div style={{animation: `y ${ySpeed}s linear infinite alternate`}}>
+            <div className="el-wrap" style={{animation: `${animationKey}-x ${xSpeed}s linear infinite alternate`}}>
+                <div style={{animation: `${animationKey}-y ${ySpeed}s linear infinite alternate`}}>
                     <Box className="el" style={{animation: `z ${zSpeed}s linear infinite`}} height={item.size} width={item.size}>
                         {item.icon}
                     </Box>
